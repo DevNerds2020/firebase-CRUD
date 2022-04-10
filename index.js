8f26e2181cb2134b82ee0c4cf618b4edb8dfab4f
DevNerds2020 <89212105+DevNerds2020@users.noreply.github.com>
2022-04-10 22:47:39 +0430

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/firestore' ;
 const firebaseConfig = {
-    apiKey: "AIzaSyDDvixkM17sWyw9XjsUZOUyBHgeC7GkuZw",
-    authDomain: "projectdbclass.firebaseapp.com",
-    projectId: "projectdbclass",
-    storageBucket: "projectdbclass.appspot.com",
-    messagingSenderId: "565133906368",
-    appId: "1:565133906368:web:9a3dbce23f845a0f127825",
-    measurementId: "G-EC9VETBXZ3"
   }  
 firebase.initializeApp(firebaseConfig);
 
